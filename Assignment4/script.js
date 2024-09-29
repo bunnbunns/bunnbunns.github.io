2709acc907d1424315bd609edef7f0fed26f50e9
bunnbunns <inkbundt@gmail.com>
2024-09-28 22:35:35 -0400

    remove console.log

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,6 +1,5 @@
 function openCloseNav() {
 	const navbox = document.getElementById("navbox");
-	console.log(navbox)
 
 	if (navbox.classList.contains('closed')) {
 		navbox.classList.remove('closed');
